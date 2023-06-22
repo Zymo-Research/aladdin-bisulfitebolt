2c6d8db23cc94f97e611320e04d274f9196c8015
laclac102 <phanvothungabiotech@gmail.com>
2023-06-22 16:00:56 +0700

    fix encoding type

--- bin/software_versions.py
+++ bin/software_versions.py
@@ -5,19 +5,19 @@ import re
 
 regexes = OrderedDict()
 
-regexes['BiSulfite pipeline'] = ['v_pipeline.txt', r"(\S+)"]
-regexes['Nextflow'] = ['v_nextflow.txt', r"(\S+)"]
-regexes['FastQC'] = ['v_fastqc.txt', r"FastQC v(\S+)"]
-regexes['Cutadapt'] = ['v_Cutadapt.txt', r"(\S+)"]
-regexes['BiSulfite Bolt'] = ['v_bsbolt.txt', r"BiSulfite Bolt v(\S+)"]
-regexes['Samtools'] = ['v_samtools.txt', r"samtools (\S+)"]
+regexes["BiSulfite pipeline"] = ["v_pipeline.txt", r"(\S+)"]
+regexes["Nextflow"] = ["v_nextflow.txt", r"(\S+)"]
+regexes["FastQC"] = ["v_fastqc.txt", r"FastQC v(\S+)"]
+regexes["Cutadapt"] = ["v_Cutadapt.txt", r"(\S+)"]
+regexes["BiSulfite Bolt"] = ["v_bsbolt.txt", r"BiSulfite Bolt v(\S+)"]
+regexes["Samtools"] = ["v_samtools.txt", r"samtools (\S+)"]
 
 results = OrderedDict()
 
 # Search each file using its regex
 for k, v in regexes.items():
     try:
-        with open(v[0]) as x:
+        with open(v[0], encoding="ISO-8859-1") as x:
             versions = x.read()
             match = re.search(v[1], versions)
             if match:
@@ -26,22 +26,24 @@ for k, v in regexes.items():
         results[k] = False
 
 # Dump to YAML
-print ('''
+print(
+    """
 id: 'software_versions'
 section_name: 'Software Versions'
 plot_type: 'html'
 description: 'Software versions are collected at run time from the software output.'
 data: |
     <dl class="dl-horizontal">
-''')
-for k,v in results.items():
+"""
+)
+for k, v in results.items():
     # Only display versions of softwares that were actually used.
     if v:
-        print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k,v))
-print ("    </dl>")
+        print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k, v))
+print("    </dl>")
 
 # Write out regexes as csv file:
-with open('software_versions.csv', 'w') as f:
-    for k,v in results.items():
+with open("software_versions.csv", "w") as f:
+    for k, v in results.items():
         if v:
-            f.write("{}\t{}\n".format(k,v))
+            f.write("{}\t{}\n".format(k, v))
